863608d2406a77a642d63c4b39d4f0d5123d2381
sadsadcool <fanzipei@iis.u-tokyo.ac.jp>
2017-04-21 09:17:24 +0900

    historical predictor first version

--- historical_predictor.py
+++ historical_predictor.py
@@ -17,25 +17,47 @@ num_locs = 1441
 T = 4
 
 
-# def read_trainingset(folderpath, min_day, max_day):
-    # for d in xrange(min_day, max_day + 1):
-        # filepath = '{}day_{}.csv'.format(d)
-        # data = np.genfromtxt(filepath, dtype=np.int32, delimiter=',')
-        # for t in xrange(96 - T - 2):
-            # data[:, t + 1:t + 1 + T]
+def get_userindex(folderpath, min_day, max_day):
+    user_set = set()
+    user_index = dict({})
+    for d in xrange(min_day, max_day + 1):
+        filepath = '{}day_{}.csv'.format(folderpath, d)
+        data = np.genfromtxt(filepath, dtype=np.int32, delimiter=',')
+        user_set = user_set.union(set(data[:, 0]))
+
+    for uid in user_set:
+        user_index[uid] = len(user_index)
+
+    return user_index
+
+
+def read_trainingset(folderpath, min_day, max_day, user_index):
+    for d in xrange(min_day, max_day + 1):
+        uX = None
+        tX = None
+        xX = None
+        filepath = '{}day_{}.csv'.format(folderpath, d)
+        data = np.genfromtxt(filepath, dtype=np.int32, delimiter=',')
+        if uX is None:
+            uX = np.vectorize(lambda x:user_index[x])(data[:, 0]).repeat(96 - T)
+        else:
+            uX = np.concatenate([uX, np.vectorize(lambda x:user_index[x])(data[:, 0]).repeat(96 - T - 2)])
+        for t in xrange(96 - T):
+            data[:, t + 1:t + 1 + T]
+
+
+user_index = get_userindex('/home/fan/work/data/dis_forhybrid/', 1, 30)
+num_users = len(user_index)
 
 
 u_input = Input(shape=(1,))
 t_input = Input(shape=(1,))
 x_input = Input(shape=(T,))
 uemb = Flatten()(Embedding(num_users, embedding_dim_user)(u_input))
-Model(u_input, uemb).summary()
 temb = Flatten()(Embedding(96, embedding_dim_time)(t_input))
 xemb = Embedding(num_locs, embedding_dim_loc, input_length=T)(x_input)
 rep_uidx = RepeatVector(T)(uemb)
-Model(u_input, rep_uidx).summary()
 rep_time = RepeatVector(T)(temb)
-# merge_input = merge((rep_uidx, rep_time, xemb), mode='concat', concat_axis=2)
 merge_input = concatenate([rep_uidx, rep_time, xemb], axis=-1)
 gru1 = GRU(hidden_dim, return_sequences=True, unroll=True, activation='softsign')(merge_input)
 gru2 = GRU(hidden_dim, return_sequences=False, unroll=True, activation='softsign')(gru1)
