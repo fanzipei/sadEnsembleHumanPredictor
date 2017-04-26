e35e9eddaf6f14db1617ec0c28efb600a11db0fb
sadsadcool <fanzipei@iis.u-tokyo.ac.jp>
2017-04-26 19:01:38 +0900

    update get in tokyo users

--- get_in_tokyo_users_trajectories.py
+++ get_in_tokyo_users_trajectories.py
@@ -2,6 +2,7 @@
 # encoding: utf-8
 
 import csv
+import os
 
 def filter_users_in_region(filename, lat_min, lat_max, lon_min, lon_max):
     user_set = dict({})
@@ -19,9 +20,9 @@ def filter_users_in_region(filename, lat_min, lat_max, lon_min, lon_max):
     return user_set
 
 
-def output_traj(in_filename, out_filename, user_set):
-    with open(in_filename, 'r') as fin:
-        with open(out_filename, 'w') as fout:
+def output_traj(full_path, out_folder, out_filename, user_set):
+    with open(full_path, 'r') as fin:
+        with open(os.path.join(out_folder, out_filename), 'w') as fout:
             for uid_str, time_str, lat_str, lon_str, _, _, in csv.reader(fin):
                 uid = int(uid_str)
                 if uid in user_set:
@@ -31,15 +32,27 @@ def output_traj(in_filename, out_filename, user_set):
                         del user_set[uid]
 
 
+def filename_generator(folder_path):
+    filename_fmt = '2012{:02d}{:02d}.csv'
+    for m in xrange(5, 6):
+        for d in xrange(1, 32):
+            filename = filename_fmt.format(m, d)
+            full_path = os.path.join(folder_path, filename)
+            print 'Reading {}'.format(full_path)
+            if os.path.isfile(full_path):
+                yield filename, full_path
+
+
 def main():
     lat_min = 35.5
     lat_max = 35.8
     lon_min = 139.4
     lon_max = 139.9
-    in_filename = '/media/fan/HDPC-UT/ZDC/2012/FeaturePhone/20120402.csv'
-    user_set = filter_users_in_region(in_filename, lat_min, lat_max, lon_min, lon_max)
-    out_filename = 'usersintokyo_20120402.csv'
-    output_traj(in_filename, out_filename, user_set)
+    folder_path = '/media/fan/HDPC-UT/ZDC/2012/FeaturePhone/'
+    for filename, full_path in filename_generator(folder_path):
+        user_set = filter_users_in_region(full_path, lat_min, lat_max, lon_min, lon_max)
+        out_folder = '/media/fan/HDPC-UT/ZDC/TrainingForMapping/usersintokyo/'
+        output_traj(full_path, out_folder, filename, user_set)
 
 
 if __name__ == '__main__':
