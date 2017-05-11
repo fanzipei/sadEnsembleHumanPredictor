276d50642cfa2dc6a5c683671c64b15c74dda988
fan <fanzipei@iis.u-tokyo.ac.jp>
2017-05-11 13:26:02 +0900

    one predictor for one month

---
+++ one_predictor.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+from keras.models import Model
+from keras.callbacks import CSVLogger, EarlyStopping, ModelCheckpoint
+from keras.layers import GRU, Embedding, Input, RepeatVector, Dense, Flatten
+from keras.layers.merge import concatenate
+from keras.optimizers import RMSprop
+
+import numpy as np
+
+embedding_dim_time = 8
+embedding_dim_loc = 64
+hidden_dim = 128
+num_locs = 1441
+batch_size = 256
+T = 4
+
+
+def read_trainingset(folderpath, d):
+    tX = None
+    xX = None
+    Y1 = None
+    Y2 = None
+    Y3 = None
+    Y4 = None
+    filepath = '{}day_{}.csv'.format(folderpath, d)
+    data = np.genfromtxt(filepath, dtype=np.int32, delimiter=',')
+    for t in xrange(96 - T - 3):
+        if tX is None:
+            tX = np.array([t] * data.shape[0])
+        else:
+            tX = np.concatenate([tX, np.array([t] * data.shape[0])])
+        if xX is None:
+            xX = data[:, t + 1:t + 1 + T]
+        else:
+            xX = np.concatenate([xX, data[:, t + 1:t + 1 + T]])
+        if Y1 is None:
+            Y1 = data[:, t + 1 + T]
+        else:
+            Y1 = np.concatenate([Y1, data[:, t + 1 + T]])
+        if Y2 is None:
+            Y2 = data[:, t + 2 + T]
+        else:
+            Y2 = np.concatenate([Y2, data[:, t + 2 + T]])
+        if Y3 is None:
+            Y3 = data[:, t + 3 + T]
+        else:
+            Y3 = np.concatenate([Y3, data[:, t + 3 + T]])
+        if Y4 is None:
+            Y4 = data[:, t + 4 + T]
+        else:
+            Y4 = np.concatenate([Y4, data[:, t + 4 + T]])
+
+    return tX, xX, Y1, Y2, Y3, Y4
+
+
+t_input = Input(shape=(1,))
+x_input = Input(shape=(T,))
+temb = Flatten()(Embedding(96 - T - 3, embedding_dim_time)(t_input))
+xemb = Embedding(num_locs, embedding_dim_loc, input_length=T)(x_input)
+rep_time = RepeatVector(T)(temb)
+merge_input = concatenate([rep_time, xemb], axis=-1)
+gru1 = GRU(hidden_dim, return_sequences=True, unroll=True, activation='softsign')(merge_input)
+gru21 = GRU(hidden_dim, return_sequences=False, unroll=True, activation='softsign')(gru1)
+gru22 = GRU(hidden_dim, return_sequences=False, unroll=True, activation='softsign')(gru1)
+gru23 = GRU(hidden_dim, return_sequences=False, unroll=True, activation='softsign')(gru1)
+gru24 = GRU(hidden_dim, return_sequences=False, unroll=True, activation='softsign')(gru1)
+shared_softmax = Dense(num_locs, activation='softmax')
+y1 = shared_softmax(gru21)
+y2 = shared_softmax(gru22)
+y3 = shared_softmax(gru23)
+y4 = shared_softmax(gru24)
+
+one_predictor = Model([t_input, x_input], [y1, y2, y3, y4])
+one_predictor.summary()
+one_predictor.compile(loss='sparse_categorical_crossentropy', optimizer=RMSprop(lr=1e-3))
+callbacks = [
+    CSVLogger('../results/sadHybridHumanPredictor/one_predictor_2011_jan/training_log.csv', separator=',', append=False),
+    ModelCheckpoint(filepath='../results/sadHybridHumanPredictor/one_predictor_2011_jan/one_predictor.hdf5', verbose=1, save_best_only=True),
+    EarlyStopping(monitor='val_loss', patience=0, verbose=1, mode='auto')
+]
+
+print 'Reading Day {}'.format(1)
+tX, xX, Y1, Y2, Y3, Y4 = read_trainingset('/home/fan/work/data/dis_forensemble_2011_jan/', 1)
+for d in xrange(2, 32):
+    print 'Reading Day {}'.format(d)
+    tX_, xX_, Y1_, Y2_, Y3_, Y4_ = read_trainingset('/home/fan/work/data/dis_forensemble_2011_jan/', d)
+    tX = np.concatenate([tX, tX_])
+    xX = np.concatenate([xX, xX_])
+    Y1 = np.concatenate([Y1, Y1_])
+    Y2 = np.concatenate([Y2, Y2_])
+    Y3 = np.concatenate([Y3, Y3_])
+    Y4 = np.concatenate([Y4, Y4_])
+
+one_predictor.fit([tX, xX], [Y1, Y2, Y3, Y4], batch_size=batch_size, epochs=20, shuffle=True,\
+                        validation_split=0.2, verbose=1, callbacks=callbacks)
