42b80dd47461037ab854ef41b9738fd11161aa84
zipei fan <fanzipei@iis.u-tokyo.ac.jp>
2017-08-31 10:37:52 +0900

    ensemble with momentum

--- ensemble_learning_with_momentum.py
+++ ensemble_learning_with_momentum.py
@@ -38,7 +38,7 @@ def build_and_load_model(model_path):
     print 'Build model {}'.format(model_path)
     t_input = Input(shape=(1,))
     x_input = Input(shape=(T,))
-    temb = Flatten()(Embedding(96 - T - 3, embedding_dim_time)(t_input))
+    temb = Flatten()(Embedding(96 - T, embedding_dim_time)(t_input))
     xemb = Embedding(num_locs, embedding_dim_loc, input_length=T)(x_input)
     rep_time = RepeatVector(T)(temb)
     merge_input = concatenate([rep_time, xemb], axis=-1)
@@ -53,7 +53,7 @@ def build_and_load_model(model_path):
     return predictor
 
 
-models = [build_and_load_model('../results/sadHybridHumanPredictor/ensemble_predictor_2011_jan/ensemble_predictor_{}.hdf5'.format(i)) for i in xrange(1, num_models + 1)]
+models = [build_and_load_model('../results/sadHybridHumanPredictor/ensemble_predictor_2010_aug/ensemble_predictor_{}.hdf5'.format(i)) for i in xrange(1, num_models + 1)]
 print 'Load Models Finished'
 
 t_input = Input(shape=(1,))
@@ -64,12 +64,16 @@ xemb_momentum = Embedding(num_locs, embedding_dim_loc, input_length=T)(x_input)
 gru_momentum_1 = GRU(hidden_dim, return_sequences=True, unroll=True, activation='softsign', dropout=0.2, recurrent_dropout=0.2)(xemb_momentum)
 gru_momentum_2 = GRU(hidden_dim, return_sequences=False, unroll=True, activation='softsign', dropout=0.2, recurrent_dropout=0.2)(gru_momentum_1)
 out_momentum = Dense(num_locs, activation='softmax')(gru_momentum_2)
-models_out = [Reshape((num_locs, 1))(model([t_input, x_input])) for model in models] + out_momentum
+
+momentum_predictor = Model(x_input, out_momentum)
+momentum_predictor.summary()
+
+models_out = [Reshape((num_locs, 1))(model([t_input, x_input])) for model in models]
+models_out.append(Reshape((num_locs, 1))(momentum_predictor(x_input)))
 pred = concatenate(models_out, axis=-1)
 weights = Reshape((num_models + 1, 1))(Dense(num_models + 1, activation='softmax')(gru1))
 y = Reshape((num_locs,))(dot([pred, weights], [2, 1]))
 
-momentum_predictor = Model(x_input, out_momentum)
 
 online_predictor = Model([t_input, x_input], y)
 online_predictor.summary()
@@ -80,13 +84,13 @@ momentum_predictor.trainable = True
 momentum_predictor.compile(loss='sparse_categorical_crossentropy', optimizer=RMSprop(lr=1e-3))
 
 for d in xrange(1, 32):
-    X = read_trainingset('/home/hpc/work/data/dis_forensemble_2012_jan/', d)
+    X = read_trainingset('/home/hpc/work/data/dis_forensemble_2012_aug/', d)
     for t in xrange(96 - T):
         callbacks = [
-            ModelCheckpoint(filepath='../results/sadHybridHumanPredictor/online_predictor_2012_jan/online_predictor_d{}t{}.hdf5'.format(d, t),\
+            ModelCheckpoint(filepath='../results/sadHybridHumanPredictor/online_predictor_with_momentum_2012_aug/online_predictor_d{}t{}.hdf5'.format(d, t),\
                             verbose=1, monitor='loss', save_best_only=True),
             EarlyStopping(monitor='loss', patience=0, verbose=1, mode='auto')
         ]
         tX, xX, Y1 = X[t]
+        momentum_predictor.fit(xX, Y1, batch_size=batch_size, epochs=25, shuffle=True, verbose=1, callbacks=[EarlyStopping(monitor='loss', patience=0, verbose=1, mode='auto')])
         online_predictor.fit([tX, xX], Y1, batch_size=batch_size, epochs=5, shuffle=True, verbose=1, callbacks=callbacks)
-        momentum_predictor.fit(xX, Y1, batch_size=batch_size, epochs=20, shuffle=True, verbose=1, callbacks=[EarlyStopping(monitor='loss', patience=0, verbose=1, mode='auto')])
