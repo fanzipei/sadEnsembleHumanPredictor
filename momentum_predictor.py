160fe8be458500935a00eb210a13e077cd3fd813
fan <fanzipei@iis.u-tokyo.ac.jp>
2017-11-01 11:54:56 -0400

    update momentum predictor

--- momentum_predictor.py
+++ momentum_predictor.py
@@ -19,17 +19,16 @@ T = 4
 
 
 def read_trainingset(folderpath, d):
+    xX = np.zeros([0, T], dtype=np.int32)
+    Y1 = np.zeros([0], dtype=np.int32)
     filepath = '{}day_{}.csv'.format(folderpath, d)
     data = np.genfromtxt(filepath, dtype=np.int32, delimiter=',')
+
     X = []
-    for t in xrange(96 - T - 3):
-        tX = np.array([t] * data.shape[0])
+    for t in xrange(96 - T):
         xX = data[:, t + 1:t + 1 + T]
         Y1 = data[:, t + 1 + T]
-        Y2 = data[:, t + 2 + T]
-        Y3 = data[:, t + 3 + T]
-        Y4 = data[:, t + 4 + T]
-        X.append((tX, xX, Y1, Y2, Y3, Y4))
+        X.append((xX, Y1))
 
     return X
 
@@ -37,37 +36,30 @@ def read_trainingset(folderpath, d):
 x_input = Input(shape=(T,))
 xemb = Embedding(num_locs, embedding_dim_loc, input_length=T)(x_input)
 gru1 = GRU(hidden_dim, return_sequences=True, unroll=True, activation='softsign')(xemb)
-gru21 = GRU(hidden_dim, return_sequences=False, unroll=True, activation='softsign')(gru1)
-gru22 = GRU(hidden_dim, return_sequences=False, unroll=True, activation='softsign')(gru1)
-gru23 = GRU(hidden_dim, return_sequences=False, unroll=True, activation='softsign')(gru1)
-gru24 = GRU(hidden_dim, return_sequences=False, unroll=True, activation='softsign')(gru1)
-shared_softmax = Dense(num_locs, activation='softmax')
-y1 = shared_softmax(gru21)
-y2 = shared_softmax(gru22)
-y3 = shared_softmax(gru23)
-y4 = shared_softmax(gru24)
+gru2 = GRU(hidden_dim, return_sequences=False, unroll=True, activation='softsign')(gru1)
+y = Dense(num_locs, activation='softmax')(gru2)
 
-momentum_predictor = Model(x_input, [y1, y2, y3, y4])
+momentum_predictor = Model(x_input, y)
 momentum_predictor.summary()
 momentum_predictor.compile(loss='sparse_categorical_crossentropy', optimizer=RMSprop(lr=2e-3))
-open('../results/sadHybridHumanPredictor/momentum_losslog_2012_jan.csv', 'w').close()
+open('../results/sadHybridHumanPredictor/momentum_losslog_2012_oct_tokyo.csv', 'w').close()
 
 for d in xrange(1, 32):
-    X = read_trainingset('/home/fan/work/data/dis_forensemble_2012_jan/', d)
-    for t in xrange(96 - 3 * T + 1):
+    X = read_trainingset('/home/fan/work/data/dis_forensemble_2012_oct_tokyo/', d)
+    for t in xrange(96 - T - 1):
         if d == 1 and t == 0:
-            max_iter = 25
+            max_iter = 50
         else:
             max_iter = 5
         callbacks = [
-            ModelCheckpoint(filepath='../results/sadHybridHumanPredictor/momentum_predictor_2012_jan/momentum_predictor_d{}t{}.hdf5'.format(d, t),\
+            ModelCheckpoint(filepath='../results/sadHybridHumanPredictor/momentum_predictor_2012_oct_tokyo/momentum_predictor_d{}t{}.hdf5'.format(d, t),
                             verbose=1, monitor='loss', save_best_only=True),
         ]
-        tX, xX, Y1, Y2, Y3, Y4 = X[t]
-        momentum_predictor.fit(xX, [Y1, Y2, Y3, Y4], batch_size=batch_size, epochs=max_iter, shuffle=True, verbose=1, callbacks=callbacks)
-        tX, xX, Y1, Y2, Y3, Y4 = X[t + T]
-        loss = momentum_predictor.evaluate(xX, [Y1, Y2, Y3, Y4], batch_size=8192)
-        with open('../results/sadHybridHumanPredictor/momentum_losslog_2012_jan.csv', 'a') as f:
+        xX, Y1 = X[t]
+        momentum_predictor.fit(xX, Y1, batch_size=batch_size, epochs=max_iter, shuffle=True, verbose=1, callbacks=callbacks)
+        xX, Y1 = X[t + 1]
+        loss = momentum_predictor.evaluate(xX, Y1, batch_size=8192)
+        with open('../results/sadHybridHumanPredictor/momentum_losslog_2012_oct_tokyo.csv', 'a') as f:
             for r in loss:
                 f.write('{},'.format(r))
             f.write('\n')
