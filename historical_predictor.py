2f8f9d2b7bb7a055419ce8c815754058aaf26911
zipei fan <fanzipei@iis.u-tokyo.ac.jp>
2020-05-28 18:30:50 +0900

    Update historical_predictor.py

--- historical_predictor.py
+++ historical_predictor.py
@@ -66,23 +66,20 @@ user_index = get_userindex('/home/fan/work/data/dis_forhybrid/', 1, 30)
 num_users = len(user_index)
 
 
-u_input = Input(shape=(1,))
 t_input = Input(shape=(1,))
 x_input = Input(shape=(T,))
-uemb = Flatten()(Embedding(num_users + 1, embedding_dim_user)(u_input))
 temb = Flatten()(Embedding(96, embedding_dim_time)(t_input))
 xemb = Embedding(num_locs, embedding_dim_loc, input_length=T)(x_input)
-rep_uidx = RepeatVector(T)(uemb)
 rep_time = RepeatVector(T)(temb)
-merge_input = concatenate([rep_uidx, rep_time, xemb], axis=-1)
+merge_input = concatenate([rep_time, xemb], axis=-1)
 gru = GRU(hidden_dim, return_sequences=False, unroll=True, activation='softsign')(merge_input)
 y = Dense(num_locs, activation='softmax')(gru)
 
-historical_predictor = Model([u_input, t_input, x_input], y)
+historical_predictor = Model([t_input, x_input], y)
 historical_predictor.summary()
 historical_predictor.compile(loss='sparse_categorical_crossentropy', optimizer=RMSprop(lr=1e-3))
 
 for uX, tX, xX, Y in read_trainingset('/home/fan/work/data/dis_forhybrid/', 1, 30, user_index, 500):
-    historical_predictor.fit([uX, tX, xX], Y, batch_size=batch_size, epochs=1)
+    historical_predictor.fit([tX, xX], Y, batch_size=batch_size, epochs=1)
     historical_predictor.save('./historical_predictor.hdf5')
     historical_predictor.save_weights('./historical_predictor_weights.hdf5')
